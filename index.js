b57e440bd4f11fb742197cb158ab5ac5f623bb8a
Apple <apple@Joaco-cukier.local>
2020-10-13 20:35:40 -0300

    segundo

---
+++ index.js
@@ -0,0 +1,29 @@
+alert ("bienvenido a mi sitio");
+
+let confirmacion = confirm ("esta seguro que desea continuar?");
+
+termina = document.querySelector ("siguiente");
+
+if (confirmacion = false {termina.innerHtml = ("que bueno que quieras seguir en este sitio"}
+
+nombre = prompt ("ingrese su nombre aqui");
+
+console.log(nombre);
+
+saludo = document.querySelector(".ingresado")
+
+saludo.innerHtml = "Bienvenido" + nombre;
+
+edad = prompt ("porfavor ingrese su edad aqui");
+
+if(edad > 18) {acceso= alert ("permitido su acceso");oscuro = confirm ("desea usar el modo oscuro?");
+
+cuerpo= document.querySelector (".mode");} 
+
+else if (edad<18) {acceso= alert("prohibido entrar");
+
+termina.style.color="white". saludo.style.color= "white";
+
+DiaDeSemana= ("domingo")
+
+if(DiaDeSemana= "domingo") {alert ("hoy no se laburo perri") }
